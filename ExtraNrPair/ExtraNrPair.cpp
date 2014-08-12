71407c45e38604ee792fc5cf55183a55c498b1d8
Bin Liu <bliu@adobe.com>
2014-08-12 22:02:36 +0800

    Init the projects

---
+++ ExtraNrPair/ExtraNrPair.cpp
@@ -0,0 +1,11 @@
+// ExtraNrPair.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
---
+++ KingCrab/KingCrab.cpp
@@ -0,0 +1,12 @@
+// KingCrab.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	return 0;
+}
+
